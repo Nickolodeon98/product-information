be0db958c6fd0261b3bcf1bfd09e0c2f0006dc8a
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-11 23:51:23 +0900

    feat: ProductService 에 리포지토리 의존성 주입

--- src/main/java/com/example/productinformation/service/ProductService.java
+++ src/main/java/com/example/productinformation/service/ProductService.java
@@ -1,7 +1,12 @@
 package com.example.productinformation.service;
 
+import com.example.productinformation.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
-
-
+  private final ProductRepository productRepository;
 
 }
