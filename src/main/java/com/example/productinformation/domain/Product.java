a31b625d120136e42dde457149d9e3334b6d4a6b
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-11 23:29:16 +0900

    feat: Product 클래스의 멤버 정의, 롬복 사용하여 getter, setter, 생성자, 빌더 등 객체 생성에 필요한 기능 추가

--- src/main/java/com/example/productinformation/domain/Product.java
+++ src/main/java/com/example/productinformation/domain/Product.java
@@ -1,7 +1,32 @@
 package com.example.productinformation.domain;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
-
-
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  // 아이디가 3억 대로 되어 있으므로 여유 있게 Long 으로 설정한다.
+  private Long itemId;
+  private String itemName;
+  private String itemImage;
+  private String itemUrl;
+  // 상품의 가격이 22억을 넘는 경우는 없으므로 int 로 설정한다.
+  private int originalPrice;
+  private int salePrice;
 }
