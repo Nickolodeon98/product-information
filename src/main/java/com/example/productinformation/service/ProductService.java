c8559416ba43a69dfd7b963be4529a24ee5823d0
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-12 00:28:37 +0900

    feat: ProductService 에 상품 등록하고 등록된 데이터 정보 리턴하는 메서드 추가

--- src/main/java/com/example/productinformation/service/ProductService.java
+++ src/main/java/com/example/productinformation/service/ProductService.java
@@ -1,12 +1,26 @@
 package com.example.productinformation.service;
 
+import com.example.productinformation.domain.Product;
+import com.example.productinformation.parser.ReadLineContext;
 import com.example.productinformation.repository.ProductRepository;
+import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+  private final ReadLineContext<Product> productReadLineContext;
+
   private final ProductRepository productRepository;
 
+
+  public ProductResponse createProduct(String filename) throws IOException {
+
+    List<Product> products = productRepository.saveAll(productReadLineContext.readLines(filename));
+
+    return ProductResponse.of(products, "상품 등록 완료");
+  }
 }
