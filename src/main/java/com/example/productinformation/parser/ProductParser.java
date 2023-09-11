3c89f9719e9a6db545b757a6aec0c557d7e81698
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-11 23:33:08 +0900

    feat: ProductParser 에서 Parser 의 구현체를 정의, product.csv 의 각 데이터 위치 보고 빌더 패턴 활용해서 Product 객체로 변환하는 메서드 정의

--- src/main/java/com/example/productinformation/parser/ProductParser.java
+++ src/main/java/com/example/productinformation/parser/ProductParser.java
@@ -1,7 +1,27 @@
 package com.example.productinformation.parser;
 
-public class ProductParser {
+import com.example.productinformation.domain.Product;
 
+public class ProductParser implements Parser<Product> {
 
+  @Override
+  public Product parse(String line) {
+    String[] row = line.split("\",\"");
 
+    final int FIRST = 0;
+    final int END = row.length - 1;
+
+    // 첫번째 컬럼과 마지막 컬럼은 변수에 넣기 전에 먼저 큰 따옴표(")를 제거해준다.
+    row[FIRST] = row[FIRST].replaceAll("\"", "");
+    row[END] = row[END].replaceAll("\"", "");
+
+    // 빌더 패턴으로 Product 객체를 생성해 바로 리턴해준다.
+    return Product.builder()
+        .itemId(Long.valueOf(row[0]))
+        .itemName(row[1])
+        .itemImage(row[2])
+        .itemUrl(row[3])
+        .originalPrice(Integer.valueOf(row[4]))
+        .salePrice(Integer.valueOf(row[5])).build();
+  }
 }
