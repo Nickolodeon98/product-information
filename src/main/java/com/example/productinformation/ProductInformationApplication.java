2caa1ac90703a676f17e6de66f83bb29e0035d9f
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-11 22:10:00 +0900

    feat: 새 프로젝트 생성

---
+++ src/main/java/com/example/productinformation/ProductInformationApplication.java
@@ -0,0 +1,13 @@
+package com.example.productinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInformationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProductInformationApplication.class, args);
+  }
+
+}
