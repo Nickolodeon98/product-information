fdfc73ef8125bf935143649dbe6129cbf8432191
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-11 23:04:40 +0900

    feat: product.csv 파일로부터 파싱을 하기 위해 Parser 인터페이스와 그 구현체 ProductParser 생성

---
+++ src/main/java/com/example/productinformation/parser/Parser.java
@@ -0,0 +1,5 @@
+package com.example.productinformation.parser;
+
+public interface Parser<T> {
+
+}
---
+++ src/main/java/com/example/productinformation/parser/ProductParser.java
@@ -0,0 +1,7 @@
+package com.example.productinformation.parser;
+
+public class ProductParser {
+
+
+
+}
