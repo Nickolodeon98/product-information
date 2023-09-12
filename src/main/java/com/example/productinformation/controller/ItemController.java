4ea367e7d0cacbacc925ec1021fc9112c54f4fbe
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-13 08:59:48 +0900

    feat: 컨트롤러 이름 ItemController 로 변경, 연관 상품 등록 API 추가

--- src/main/java/com/example/productinformation/controller/ItemController.java
+++ src/main/java/com/example/productinformation/controller/ItemController.java
@@ -1,9 +1,9 @@
 package com.example.productinformation.controller;
 
-import com.example.productinformation.domain.dto.request.ProductRequest;
+import com.example.productinformation.domain.dto.request.FileRequest;
 import com.example.productinformation.domain.dto.response.ProductResponse;
 import com.example.productinformation.domain.Response;
-import com.example.productinformation.service.ProductService;
+import com.example.productinformation.service.ItemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,22 +17,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/products")
+@RequestMapping("/items")
 @RequiredArgsConstructor
 @Slf4j
 @Api(tags="상품 CRUD 엔드포인트")
 @ApiOperation("상품 관련 API 의 컨트롤러")
-public class ProductController {
-
-  private final ProductService productService;
+public class ItemController {
+  private final ItemService itemService;
 
   @ResponseBody
-  @Operation(summary="상품 등록", description = "주어지는 파일 이름으로부터 상품 데이터를 추출하여 DB에 저장할 수 있다.")
+  @Operation(summary="상품 등록", description = "입력된 파일 이름으로부터 상품 데이터를 추출하여 DB에 저장할 수 있다.")
   @PostMapping("/new")
-  public Response<ProductResponse> registerItems(@RequestBody ProductRequest productRequest)
+  public Response<ProductResponse> registerItems(@RequestBody FileRequest fileRequest)
       throws IOException {
-    ProductResponse productResponse = productService.createProduct(productRequest);
+    ProductResponse productResponse = itemService.createProduct(fileRequest);
 
     return Response.success(productResponse);
   }
+
+  @ResponseBody
+  @Operation(summary="연관 상품 등록", description = "입력된 파일 이름으로부터 연관 상품 데이터를 추출하여 DB에 저장할 수 있다.")
+  @PostMapping("/relevance")
+  public Response<RecommendResponse> registerRecommends(@RequestBody FileRequest fileRequest) {
+
+    return Response.success(recommendResponse)
+  }
+
 }
