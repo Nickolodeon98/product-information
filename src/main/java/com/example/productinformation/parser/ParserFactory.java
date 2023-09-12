0fae50175024ce39eb1230366e06ae511a05f84c
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-12 23:42:26 +0900

    feat: 데이터를 추출해 파싱하여 Recommend 엔티티에 넣어주는 메서드를 스프링 빈으로 정의함

--- src/main/java/com/example/productinformation/parser/ParserFactory.java
+++ src/main/java/com/example/productinformation/parser/ParserFactory.java
@@ -1,14 +1,23 @@
 package com.example.productinformation.parser;
 
 import com.example.productinformation.domain.entity.Product;
+import com.example.productinformation.domain.entity.Recommend;
+import com.example.productinformation.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ParserFactory {
-
+  private final ProductRepository productRepository;
   @Bean
   public ReadLineContext<Product> productReadLineContext() {
     return new ReadLineContext<Product>(new ProductParser());
+  }
+
+  @Bean
+  public ReadLineContext<Recommend> recommendReadLineContext() {
+    return new ReadLineContext<Recommend>(new RecommendParser(productRepository));
   }
 }
