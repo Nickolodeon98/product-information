ef34d686bd2aff745778fe8b568b5c74a9c94053
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-13 20:56:36 +0900

    feat: 연관 관계에 있는 상품의 정보로 연관 상품 정보를 알아내는 findAllByTarget 메서드 추가

--- src/main/java/com/example/productinformation/repository/RecommendRepository.java
+++ src/main/java/com/example/productinformation/repository/RecommendRepository.java
@@ -1,9 +1,13 @@
 package com.example.productinformation.repository;
 
+import com.example.productinformation.domain.entity.Product;
 import com.example.productinformation.domain.entity.Recommend;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecommendRepository extends JpaRepository<Recommend, Long> {
+
+  List<Recommend> findAllByTarget(Product product);
 }
