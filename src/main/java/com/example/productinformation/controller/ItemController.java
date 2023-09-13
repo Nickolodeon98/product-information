dc63c92de72e523e3f07377b6e236fe65c4186ff
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-13 21:24:53 +0900

    style: 정렬 변화

--- src/main/java/com/example/productinformation/controller/ItemController.java
+++ src/main/java/com/example/productinformation/controller/ItemController.java
@@ -24,13 +24,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/rec")
 @RequiredArgsConstructor
 @Slf4j
-@Api(tags="상품 CRUD 엔드포인트")
+@Api(tags = "상품 CRUD 엔드포인트")
 @ApiOperation("상품 관련 API 의 컨트롤러")
 public class ItemController {
+
   private final ItemService itemService;
 
   @ResponseBody
-  @Operation(summary="상품 등록", description = "입력된 파일 이름으로부터 상품 데이터를 추출하여 DB에 저장할 수 있다.")
+  @Operation(summary = "상품 등록", description = "입력된 파일 이름으로부터 상품 데이터를 추출하여 DB에 저장할 수 있다.")
   @PostMapping("/items/new")
   public Response<ProductResponse> registerItems(@RequestBody FileRequest fileRequest)
       throws IOException {
@@ -40,7 +41,7 @@ public class ItemController {
   }
 
   @ResponseBody
-  @Operation(summary="연관 상품 등록", description = "입력된 파일 이름으로부터 연관 상품 데이터를 추출하여 DB에 저장할 수 있다.")
+  @Operation(summary = "연관 상품 등록", description = "입력된 파일 이름으로부터 연관 상품 데이터를 추출하여 DB에 저장할 수 있다.")
   @PostMapping("/items/relevance")
   public Response<RecommendResponse> registerRecommends(@RequestBody FileRequest fileRequest)
       throws IOException {
@@ -51,7 +52,7 @@ public class ItemController {
   }
 
   @ResponseBody
-  @Operation(summary="상품 조회", description = "상품 정보 및 연관 상품에 대한 조회")
+  @Operation(summary = "상품 조회", description = "상품 정보 및 연관 상품에 대한 조회")
   @GetMapping
   public Response<ItemResponse> searchItems(@RequestParam("id") String itemId) {
 
@@ -60,7 +61,4 @@ public class ItemController {
 
     return Response.success(itemResponse);
   }
-
-
-
 }
--- src/main/java/com/example/productinformation/domain/entity/Recommend.java
+++ src/main/java/com/example/productinformation/domain/entity/Recommend.java
@@ -24,7 +24,6 @@ public class Recommend {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private Long itemId;
 
   @ManyToOne
