ce776eb282c984c419188fa86bb2c396e3f0490e
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-13 09:00:07 +0900

    feat: 연관 상품 등록 서비스 로직 메서드 작성 완료

---
+++ src/main/java/com/example/productinformation/service/ItemService.java
@@ -0,0 +1,40 @@
+package com.example.productinformation.service;
+
+import com.example.productinformation.domain.entity.Product;
+import com.example.productinformation.domain.dto.request.FileRequest;
+import com.example.productinformation.domain.dto.response.ProductResponse;
+import com.example.productinformation.domain.entity.Recommend;
+import com.example.productinformation.parser.ReadLineContext;
+import com.example.productinformation.repository.ProductRepository;
+import com.example.productinformation.repository.RecommendRepository;
+import java.io.IOException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+  private final ReadLineContext<Product> productReadLineContext;
+
+  private final ReadLineContext<Recommend> recommendReadLineContext;
+  private final ProductRepository productRepository;
+
+  private final RecommendRepository recommendRepository;
+
+
+  public ProductResponse createProduct(FileRequest fileRequest) throws IOException {
+
+    List<Product> products = productRepository.saveAll(productReadLineContext.readLines(fileRequest.getFilename()));
+
+    return ProductResponse.of(products, "등록 완료");
+  }
+
+  public RecommendResponse createRecommend(FileRequest fileRequest) throws IOException {
+
+    List<Recommend> recommends = recommendRepository.saveAll(recommendReadLineContext.readLines(fileRequest.getFilename()));
+
+    return RecommendResponse.of(recommends, "등록 완료");
+  }
+}
