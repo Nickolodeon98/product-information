e81c355d5a4889d95c3a7025233c315494b76c5a
Nickolodeon98 <sjeon0730@gmail.com>
2023-09-13 23:46:38 +0900

    feat: 상품의 모든 상세 정보를 담는 DTO 생성

---
+++ src/main/java/com/example/productinformation/domain/dto/DetailedInfo.java
@@ -0,0 +1,39 @@
+package com.example.productinformation.domain.dto;
+
+import com.example.productinformation.domain.entity.Product;
+import com.example.productinformation.domain.entity.Recommend;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DetailedInfo {
+
+  private Long itemId;
+  private String itemName;
+  private String itemImage;
+  private String itemUrl;
+  private Integer originalPrice;
+  private Integer salePrice;
+  private Integer score;
+  private Integer ranking;
+
+  public static DetailedInfo of(Product productInfo, Recommend recommend) {
+    return DetailedInfo.builder()
+        .itemId(productInfo.getItemId())
+        .itemName(productInfo.getItemName() == null ? null : productInfo.getItemName())
+        .itemImage(productInfo.getItemImage() == null ? null : productInfo.getItemImage())
+        .itemUrl(productInfo.getItemUrl() == null ? null : productInfo.getItemUrl())
+        .originalPrice(productInfo.getOriginalPrice() == null ? null : productInfo.getOriginalPrice())
+        .salePrice(productInfo.getSalePrice() == null ? null : productInfo.getSalePrice())
+        .score(recommend.getScore())
+        .ranking(recommend.getRanking())
+        .build();
+  }
+}
